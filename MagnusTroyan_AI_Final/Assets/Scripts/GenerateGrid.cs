a576f42e9a4be17eda737a4d393d6c89027f8b20
Nick Magnus <nickjmagnus@gmail.com>
2018-12-04 10:38:07 -0500

    making my first mistake

---
+++ MagnusTroyan_AI_Final/Assets/Scripts/GenerateGrid.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateGrid : MonoBehaviour
+{
+
+
+    public Vector2 GRidSize;
+
+    void Start()
+    {
+        for (int x = 0; x < GridSize.x; x++)
+        {
+            for (int y = 0; y < GridSize.y; y++)
+            {
+                //create node
+            }
+        }
+    }
+
+
+
+}
---
+++ MagnusTroyan_AI_Final/Assets/Scripts/Node.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    [SerializeField]
+    float Weight = 0.0f;
+
+    void Start()
+    {
+
+    }
+
+
+    float getWeight() { return Weight; }
+    void setWeight(float newWeight)
+    {
+        Weight = newWeight;
+    }
+
+}
